cbccd925c0807e2455537e84155763adef07be9d
DenisKostyukov <kostukov08@gmail.com>
2021-07-26 14:58:58 +0300

    add services

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './common/style/reset.css'
 import Header from './components/Header'
 import MainPage from './components/MainPage'
+import Portfolio from './components/Portfolio'
 import Services from './components/Services'
 function App () {
   return <> 
   <Header />
   <MainPage />
   <Services />
+  <Portfolio />
   </>
 }
 
