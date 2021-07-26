8563e93d77e573cbc860b2727957455f5781c33b
DenisKostyukov <kostukov08@gmail.com>
2021-07-26 17:58:02 +0300

    add block about

--- src/components/About/AboutItem/index.jsx
+++ src/components/About/AboutItem/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import style from './AboutItem.module.sass'
-function AboutItem() {
+import React from 'react';
+import style from './AboutItem.module.sass';
+function AboutItem ({ image, date, title, description }) {
   return (
     <>
-
+      <div className={style.aboutItem}>
+        <img src={image} alt='' className={style.image} />
+        <div className={style.content}>
+          <h2 className={style.date}>{date}</h2>
+          <h2 className={style.title}>{title}</h2>
+          <p className={style.description}>{description}</p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default AboutItem
+export default AboutItem;
--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import BlockDescription from '../BlockDescription';
 import BlockTitle from '../BlockTitle';
 import '../../common/style/mainStyle.sass';
+import first from './images/1.jpg';
+import second from './images/2.jpg';
+import third from './images/3.jpg';
+import fourth from './images/4.jpg';
 import style from './About.module.sass';
+import AboutItem from './AboutItem';
 function About () {
   return (
     <>
@@ -10,7 +15,34 @@ function About () {
         <div className='container'>
           <div className={style.aboutInner}>
             <BlockTitle title='about' />
-            <BlockDescription descrition="Lorem ipsum dolor sit amet consectetur." />
+            <BlockDescription descrition='Lorem ipsum dolor sit amet consectetur.' />
+            <div className={style.aboutItems}>
+              <AboutItem
+                image={first}
+                date='2009-2011'
+                title='Our Humble Beginnings'
+                description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!'
+              />
+              <AboutItem
+                image={second}
+                date='March 2011'
+                title='An Agency is Born'
+                description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!'
+              />
+              <AboutItem
+                image={third}
+                date='December 2015'
+                title='Transition to Full Service'
+                description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!'
+              />
+              <AboutItem
+                image={fourth}
+                date='July 2020'
+                title='Phase Two Expansion'
+                description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!'
+              />
+            </div>
+            <div className={style.lastItem}>Be Part Of Our Story!</div>
           </div>
         </div>
       </section>
