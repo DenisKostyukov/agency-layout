964bdead5f865c24f9c3ad198384b2d9e8e143fe
DenisKostyukov <kostukov08@gmail.com>
2021-07-26 15:37:41 +0300

    add scroll styles for header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import style from './Header.module.sass';
 import Logo from './Logo';
 import NavMenu from './NavMenu';
 function Header () {
+  const [header, setHeader] = useState("header")
+  const scrollEvent = e =>{
+    if(window.scrollY){
+      return setHeader("scrolledHeader")
+    }else{
+      return setHeader("header")
+    }
+  }
+  useEffect(()=>{
+    window.addEventListener("scroll",scrollEvent)
+    return ()=> window.removeEventListener("scroll", scrollEvent)
+  },[])
   return (
     <>
-      <header className={style.header}>
+      <header className={style[header]}>
         <div className={style.container}>
           <div className={style.headerInner}>
             <Logo />
